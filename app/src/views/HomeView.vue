4e62dacea6ac9848aed86b997cc8de2aab33da79
WilsonD09 <wilsond34@nycstudents.net>
2025-03-03 12:31:54 -0500

    s

--- app/src/views/HomeView.vue
+++ app/src/views/HomeView.vue
@@ -1,5 +1,18 @@
-<script setup></script>
-
 <template>
-  <main></main>
+  <div></div>
 </template>
+
+<script setup>
+import { ref, onMounted } from 'vue'
+const airData = ref('')
+async function getData() {
+  let response = await fetch('https://data.cityofnewyork.us/resource/dsyc-npkh.json')
+  let data = await response.json()
+  airData.value = data.results
+}
+onMounted(() => {
+  getData()
+})
+</script>
+
+<style scoped></style>
