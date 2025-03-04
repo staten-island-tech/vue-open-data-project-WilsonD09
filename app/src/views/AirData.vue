6804516c4798f3515d3d8cc37112030a8bd383cc
WilsonD09 <wilsond34@nycstudents.net>
2025-03-04 12:17:50 -0500

    Create AirData.vue

---
+++ app/src/views/AirData.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <h2>{{ pokemon.name }}</h2>
+  </div>
+</template>
+
+<script setup>
+import { ref, onMounted } from 'vue'
+import { useRoute } from 'vue-router'
+const route = useRoute()
+const airData = ref('')
+async function getData() {
+  let response = await fetch(
+    `https://data.cityofnewyork.us/resource/dsyc-npkh.json/${route.params.id}`,
+  )
+  let data = await response.json()
+  airData.value = data
+}
+
+onMounted(() => {
+  getData()
+})
+</script>
+
+<style scoped></style>
